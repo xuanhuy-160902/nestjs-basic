99b794ed4ba2ac65308d9f2b5ac37cee4014543f
Dương Xuân Huy <duong.xuan.huy.16.09.2002@gmail.com>
2023-09-11 05:20:39 +0700

    Thêm người dùng mới

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  phone: number;
+
+  @Prop()
+  age: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+  // create(createUserDto: CreateUserDto) {
+  //   return 'This action adds a new user';
+  // }
+
+  getPasswordHash(password: string) {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    createUserDto.password = this.getPasswordHash(createUserDto.password);
+    const user = await this.userModel.create(createUserDto);
+    return user;
   }
 
   findAll() {
