bd6c19ef8cba9b260d57b7f03d4cb63f53b2437b
Dương Xuân Huy <duong.xuan.huy.16.09.2002@gmail.com>
2023-09-19 18:32:07 +0700

    Bật tính năng CORS

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,12 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
