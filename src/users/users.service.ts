1a3e1d64890271b882e2e008ebeffbe93ecde974
Dương Xuân Huy <duong.xuan.huy.16.09.2002@gmail.com>
2023-09-20 01:49:48 +0700

    Sửa mã dư ở users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,7 +13,6 @@ export class UsersService {
     @InjectModel(User.name)
     private readonly userModel: SoftDeleteModel<UserDocument>,
   ) {}
-  x;
   // create(createUserDto: CreateUserDto) {
   //   return 'This action adds a new user';
   // }
@@ -28,39 +27,33 @@ export class UsersService {
     return compareSync(password, pwHash);
   }
 
-  async create(createUserDto: CreateUserDto) {
-    createUserDto.password = this.getPasswordHash(createUserDto.password);
-    const user = await this.userModel.create(createUserDto);
-    return user;
+  create(createUserDto: CreateUserDto) {
+    const password = createUserDto.password;
+    const passwordHash = this.getPasswordHash(password);
+    createUserDto.password = passwordHash;
+    return this.userModel.create(createUserDto);
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  async findOne(id: string) {
+  findOne(id: string) {
     if (!isValidObjectId(id)) return `user not found`;
-    return await this.userModel.findOne({
-      _id: id,
-    });
+    return this.userModel.findOne({ _id: id });
   }
 
-  async findOneByUsername(username: string) {
-    return await this.userModel.findOne({
-      email: username,
-    });
+  findOneByUsername(username: string) {
+    return this.userModel.findOne({ email: username });
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto) {
+  update(id: string, updateUserDto: UpdateUserDto) {
     if (!isValidObjectId(id)) return `user not found`;
-    const result = await this.userModel.updateOne({ _id: id }, updateUserDto);
-    return result;
+    return this.userModel.updateOne({ _id: id }, updateUserDto);
   }
 
   remove(id: string) {
     if (!isValidObjectId(id)) return `user not found`;
-    return this.userModel.softDelete({
-      _id: id,
-    });
+    return this.userModel.softDelete({ _id: id });
   }
 }
